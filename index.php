bd9eeb9a961aa73f87fe6d7392928fe42895a224
Streetcode <72893820+streetcodemedia@users.noreply.github.com>
2020-11-11 20:11:18 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php get_header(header.php); ?>
 
 <div class="containter-fluid">
-
+<section class="slider-container">
 	<div class="col-12 cloud-slider">
 		<img src="//www.implementingchange.com/wp-content/uploads/2016/06/page-bg-1.jpg">
 		<div class="col-12 slider-bottom">
 		</div>
 	</div>
+</section>
 
+<section class="main-content-container">
 	<div class="col-12 content-section-one">	
 	</div>
 
@@ -16,7 +18,7 @@
 
 <div class="col-12 content-section-three">
 </div>
-
+</section>
 
 
 
